2712d6b86131bdacb5740c289c6247e72317f915
DebbieWilliams <willsdeb55@gmsil.com>
2023-12-18 05:50:22 -0800

    fulfilled

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    list_count = 0
+    for a in range(x):
+        try:
+            print("{:d}".format(my_list[a]), end="")
+            list_count += 1
+        except (ValueError, TypeError):
+            pass
+    print()
+    return (list_count)
